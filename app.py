fe07d8c005aea6c9522cdece32cdeaaa96460b30
yususffirman <jatmokosutresno@gmail.com>
2024-09-10 10:00:33 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,16 @@
+def luas_segitiga():
+    a = int(input("masukan alas segitiga: "))
+    t = int(input("masukan tinggi segitiga: "))
+    luas = a*t/2
+    print("luas segitiga adalah: ", luas)
+2
+luas_segitiga()
+
+def luas_persegi_panjang():
+    p = int(input("masukan panjang persegi: "))
+    l = int(input("masukan lebar persegi: "))
+    luas = p * l
+    print("luas persegi adalah: ", luas)
+
+luas_persegi_panjang()
+
